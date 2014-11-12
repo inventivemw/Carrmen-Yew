171b3009d8247b0fac9c34411ba5a6f223993c1d
Daniel Ngo <danieln@inventivemw.com>
2014-11-13 00:03:05 +1100

    added menu in default pages

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,12 +1,17 @@
 <?php snippet('header') ?>
+<div id="main">
+  <div id="wrapper">
+    <div id="content-wrapper">
+      <header id="top">
+          <h1><a href="/">Carrmen Yew</a></h1>
+          <?php snippet('menu') ?>
+      </header>
 
-  <div id="page-wrapper">
-
-    <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
-      <?php echo $page->text()->kirbytext() ?>
-    </div>
-
+	    <div class="text">
+	      <h1><?php echo $page->title()->html() ?></h1>
+	      <?php echo $page->text()->kirbytext() ?>
+	    </div>
+  	</div>
   </div>
-
+</div>
 <?php snippet('footer') ?>
